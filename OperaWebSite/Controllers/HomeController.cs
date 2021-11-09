9e906449b91db74b0ab519dde48b6c206c227484
franarci <fran.arciniega96@gmail.com>
2021-11-09 12:31:52 -0300

    HomeController

---
+++ OperaWebSite/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using OperaWebSite.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OperaWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [MyFilterAction]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
